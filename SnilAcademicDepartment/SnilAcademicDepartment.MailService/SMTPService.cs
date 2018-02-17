a33813c754d527bad8213aec2594ca9839caa499
Yury_Kabernik-Berazouski <Yury_Kabernik-Berazouski@epam.com>
2018-02-17 22:04:28 +0300

    Fix ruleset and MailSender.cs

---
+++ SnilAcademicDepartment/SnilAcademicDepartment.MailService/SMTPService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace MailService
+{
+    /// <summary>
+    /// SMTP Service for sending the confirmation link to new user.
+    /// </summary>
+    public sealed class SMTPService
+    {
+        public void SendEmail(MailMessage message)
+        {
+            ArgumentValidation(message);
+            SendMail(message);
+        }
+
+        private static void ArgumentValidation(MailMessage message)
+        {
+            if (message.To.Count == 0)
+                throw new ArgumentNullException("message", "Address doesn't exist in this mail.");
+
+            if (string.IsNullOrEmpty(message.Body))
+                throw new ArgumentNullException("message", "Mail body is null or empty.");
+
+            if (string.IsNullOrEmpty(message.Subject))
+                throw new ArgumentNullException("message", "Mail subject is null or empty.");
+        }
+
+        private void SendMail(MailMessage mailMessage)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                try
+                {
+                    smtpClient.Send(mailMessage);
+                }
+                catch (SmtpFailedRecipientsException ex)
+                {
+                    var args = new SmtpEventArgs(ex);
+                    //TODO: For logging this exception
+                }
+                catch (SmtpException ex)
+                {
+                    var args = new SmtpEventArgs(ex);
+                    //TODO: For logging this exception
+                }
+            }
+        }
+
+    }
+}
---
+++ SnilAcademicDepartment/SnilAcademicDepartment.MailService/SmtpEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailService
+{
+    internal class SmtpEventArgs : EventArgs
+    {
+        public Exception ExceptionExemplar { get; }
+
+        public SmtpEventArgs(Exception exception)
+        {
+            this.ExceptionExemplar = exception;
+        }
+    }
+}
