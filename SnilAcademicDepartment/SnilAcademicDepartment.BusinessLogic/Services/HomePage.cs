d57b6eba4fb01fb621c1d94bc8e3a43ec0b61337
Yury Kabernik-Berazouski <Yury_Kabernik-Berazouski@epam.com>
2018-04-19 13:31:30 +0300

    Add HomePage service

---
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Services/HomePage.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using SnilAcademicDepartment.BusinessLogic.Interfaces;
+using SnilAcademicDepartment.BusinessLogic.Models;
+using SnilAcademicDepartment.DataAccess.Interface;
+
+namespace SnilAcademicDepartment.BusinessLogic.Services
+{
+    class HomePage : IIndex
+    {
+        private readonly ILogger _logger;
+        private readonly IRepository _repository;
+
+        public HomePage(ILogger logger, IRepository repository)
+        {
+            this._logger = logger;
+            this._repository = repository;
+        }
+
+        public PreView GetIndexPreRolls()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PreView GetIndexPreViews()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
