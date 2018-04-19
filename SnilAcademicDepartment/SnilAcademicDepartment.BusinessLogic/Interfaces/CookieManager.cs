1945a0cd0797b27ddccfbe59e7f2b62770ea47af
Yury Kabernik-Berazouski <Yury_Kabernik-Berazouski@epam.com>
2018-04-19 13:52:38 +0300

    Add CookieManager constructor

--- SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Interfaces/CookieManager.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Interfaces/CookieManager.cs
@@ -1,4 +1,6 @@
-﻿using SnilAcademicDepartment.BusinessLogic.Interfaces;
+﻿using NLog;
+using SnilAcademicDepartment.BusinessLogic.Interfaces;
+using SnilAcademicDepartment.DataAccess.Interface;
 using System;
 using System.Web;
 
@@ -6,6 +8,15 @@ namespace SnilAcademicDepartment.BusinessLogic.Services
 {
     class CookieManager : ICookieManager
     {
+        private readonly ILogger _logger;
+        private readonly IRepository _repository;
+
+        public CookieManager(ILogger logger, IRepository repository)
+        {
+            this._logger = logger;
+            this._repository = repository;
+        }
+        
         public HttpCookie ChangeCulture(string language)
         {
             throw new NotImplementedException();
