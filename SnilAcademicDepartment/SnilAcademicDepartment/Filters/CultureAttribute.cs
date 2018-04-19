1effbb16cbdffb3c1b6e1f009b08bed19e9aeb67
Yury Kabernik-Berazouski <Yury_Kabernik-Berazouski@epam.com>
2018-04-19 11:56:45 +0300

    Edit Culture attribute

--- SnilAcademicDepartment/SnilAcademicDepartment/Filters/CultureAttribute.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment/Filters/CultureAttribute.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Globalization;
 using System.Threading;
 using System.Web;
@@ -9,28 +8,86 @@ namespace SnilAcademicDepartment.Filters
 {
     public class CultureAttribute : FilterAttribute, IActionFilter
     {
+        // Список культур
+        public readonly List<string> cultureCollection = new List<string>() { "en", "ru", "de" };
+
+        /// <summary>
+        /// Culture filter on action executed culture.
+        /// </summary>
+        /// <param name="filterContext"> Action executed context.</param>
         public void OnActionExecuted(ActionExecutedContext filterContext)
         {
-            string cultureName = null;
-            // Получаем куки из контекста, которые могут содержать установленную культуру
-            HttpCookie cultureCookie = filterContext.HttpContext.Request.Cookies["lang"];
-            if (cultureCookie != null)
-                cultureName = cultureCookie.Value;
-            else
-                cultureName = "ru";
+            var cultureName = this.CheckCookieCulture(filterContext.HttpContext);
 
-            // Список культур
-            List<string> cultures = new List<string>() { "ru", "en", "de" };
-            if (!cultures.Contains(cultureName))
+            if (string.IsNullOrEmpty(cultureName))
             {
-                cultureName = "ru";
+                cultureName = this.CheckHeaderCulture(filterContext.HttpContext);
             }
+
+            if (string.IsNullOrEmpty(cultureName))
+            {
+                cultureName = "en";
+            }
+
             Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureName);
             Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(cultureName);
         }
         public void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            //не реализован
+            // throw new NotImplementedException("Method is not implemented.");
+        }
+
+        /// <summary>
+        /// Method of finding a culture in cookies.
+        /// </summary>
+        /// <param name="httpContext">Base request context.</param>
+        /// <returns>The string of current request culture.</returns>
+        private string CheckCookieCulture(HttpContextBase httpContext)
+        {
+            string currentCulture = string.Empty;
+            
+            // Получаем куки из контекста, которые могут содержать установленную культуру
+            var cultureCookie = httpContext.Request.Cookies["lang"];
+
+            if (cultureCookie != null)
+                currentCulture = cultureCookie.Value;
+            else
+                return null;
+
+            // Поиск культуры из списка культур
+            if (!cultureCollection.Contains(currentCulture))
+            {
+                return null;
+            }
+
+            return currentCulture;
+        }
+
+        /// <summary>
+        /// Method of finding a culture in request headers.
+        /// </summary>
+        /// <param name="httpContext">Base request context.</param>
+        /// <returns>The string of current request culture.</returns>
+        private string CheckHeaderCulture(HttpContextBase httpContext)
+        {
+            var currentCulture = string.Empty;
+
+            // Получаем заголовок из запроса, который может содержать установленную культуру
+            var cultureHeader = httpContext.Request.Headers["Accept-Language"];
+
+            if (!string.IsNullOrEmpty(cultureHeader))
+                currentCulture = cultureHeader;
+
+            // Поиск культуры из списка культур
+            foreach (var lang in cultureCollection)
+            {
+                if (currentCulture.Contains(lang))
+                {
+                    return lang;
+                }
+            }
+
+            return currentCulture;
         }
     }
 }
