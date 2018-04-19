43d9ce0003d317e7686dec6b02ace5fadab6ecc3
Yury Kabernik-Berazouski <Yury_Kabernik-Berazouski@epam.com>
2018-04-19 13:29:13 +0300

    Add preview block model

---
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Models/PreView.cs
@@ -0,0 +1,14 @@
+﻿namespace SnilAcademicDepartment.BusinessLogic.Models
+{
+    /// <summary>
+    /// Model of preview page block.
+    /// </summary>
+    public class PreView
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int[] Image { get; set; }
+    }
+}
