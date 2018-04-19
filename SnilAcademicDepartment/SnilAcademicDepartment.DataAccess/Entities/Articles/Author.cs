a3b865f55230cd61f2eee1d2c8a0a1f2138a6bb4
Yury Kabernik-Berazouski <Yury_Kabernik-Berazouski@epam.com>
2018-04-19 16:47:27 +0300

    Revert operation 1

---
+++ SnilAcademicDepartment/SnilAcademicDepartment.DataAccess/Entities/Articles/Author.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace SnilAcademicDepartment.DataAccess.Entities
+{
+    /// <summary>
+    /// Model of authors.
+    /// </summary>
+    public class Author
+    {
+        /// <summary>
+        ///  Gets and sets author id.
+        /// </summary>
+        public string AuthorId { get; set; }
+        /// <summary>
+        /// Gets and sets author's first name.
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Gets and sets author's secound name.
+        /// </summary>
+        public string SecoundName { get; set; }
+        /// <summary>
+        /// Gets and sets author's position in SNIL academic department.
+        /// </summary>
+        public string Position { get; set; }
+        /// <summary>
+        /// Gers and sets author's articles. 
+        /// </summary>
+        public ICollection<Article> Articles { get; set; }
+    }
+}
---
+++ SnilAcademicDepartment/SnilAcademicDepartment.DataAccess/Entities/EducationBlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace SnilAcademicDepartment.DataAccess.Entities
+{
+    /// <summary>
+    /// Entity of the education block.
+    /// </summary>
+    public class EducationBlock
+    {
+        /// <summary>
+        /// Gets and sets education block id.
+        /// </summary>
+        public virtual string Id { get; set; }
+        /// <summary>
+        /// Gets and sets picture of education block.
+        /// </summary>
+        public virtual Image Picture { get; set; }
+        /// <summary>
+        /// Gets and sets shot discription of education block.
+        /// </summary>
+        public virtual string ShortDiscription { get; set; }
+        /// <summary>
+        /// Gets and sets tags of education block.
+        /// </summary>
+        public virtual ICollection<string> Tags { get; set; }
+    }
+}
