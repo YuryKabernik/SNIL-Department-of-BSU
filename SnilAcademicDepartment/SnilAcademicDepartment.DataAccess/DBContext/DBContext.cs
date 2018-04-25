81e2003e208317aaae14680e6cde498c140852b6
KobernykYura <kobernykbeljr@gmail.com>
2018-04-25 13:09:27 +0300

    Edit DBContext

--- SnilAcademicDepartment/SnilAcademicDepartment.DataAccess/DBContext/DBContext.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment.DataAccess/DBContext/DBContext.cs
@@ -1,4 +1,5 @@
 using System.Data.Entity;
+using SnilAcademicDepartment.DataAccess.Configurations;
 
 namespace SnilAcademicDepartment.DataAccess
 {
@@ -32,129 +33,17 @@ namespace SnilAcademicDepartment.DataAccess
         {
             base.OnModelCreating(modelBuilder);
 
-            // TODO: Remove into Configurations folder.
-            modelBuilder.Entity<Biography>()
-                .HasMany(e => e.People)
-                .WithRequired(e => e.Biography1)
-                .HasForeignKey(e => e.Biography)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Document>()
-                .Property(e => e.DocumentName)
-                .IsFixedLength();
-
-            modelBuilder.Entity<Document>()
-                .Property(e => e.FileContent)
-                .IsFixedLength();
-
-            modelBuilder.Entity<Document>()
-                .HasMany(e => e.Seminars)
-                .WithOptional(e => e.Document)
-                .HasForeignKey(e => e.DocFile);
-
-            modelBuilder.Entity<EducationBlock>()
-                .Property(e => e.Description)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<EducationBlock>()
-                .HasMany(e => e.EducationTopics)
-                .WithRequired(e => e.EducationBlock)
-                .HasForeignKey(e => e.EducationBlockType)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Image>()
-                .HasMany(e => e.EducationBlocks)
-                .WithRequired(e => e.Image1)
-                .HasForeignKey(e => e.Image)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Language>()
-                .HasMany(e => e.EducationBlocks)
-                .WithRequired(e => e.Language)
-                .HasForeignKey(e => e.Localisation)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Language>()
-                .HasMany(e => e.Lectures)
-                .WithRequired(e => e.Language)
-                .HasForeignKey(e => e.Localisation)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Language>()
-                .HasMany(e => e.People)
-                .WithRequired(e => e.Language)
-                .HasForeignKey(e => e.Localisation)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Language>()
-                .HasMany(e => e.PreViews)
-                .WithOptional(e => e.Language)
-                .HasForeignKey(e => e.Localisation);
-
-            modelBuilder.Entity<Language>()
-                .HasMany(e => e.Projects)
-                .WithRequired(e => e.Language)
-                .HasForeignKey(e => e.Localisation)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Language>()
-                .HasMany(e => e.Seminars)
-                .WithRequired(e => e.Language)
-                .HasForeignKey(e => e.Localisation)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Language>()
-                .HasMany(e => e.Specialisations)
-                .WithRequired(e => e.Language)
-                .HasForeignKey(e => e.Localiation)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Lecture>()
-                .HasMany(e => e.PersonLectures)
-                .WithRequired(e => e.Lecture)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<PageType>()
-                .HasMany(e => e.PreViews)
-                .WithRequired(e => e.PageType1)
-                .HasForeignKey(e => e.PageType)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Person>()
-                .HasMany(e => e.HallOfFames)
-                .WithRequired(e => e.Person)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Person>()
-                .HasMany(e => e.PersonLectures)
-                .WithRequired(e => e.Person)
-                .HasForeignKey(e => e.LectureId)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Person>()
-                .HasMany(e => e.Projects)
-                .WithMany(e => e.People)
-                .Map(m => m.ToTable("ProjectPerson").MapLeftKey("PersonId").MapRightKey("ProjectId"));
-
-            modelBuilder.Entity<Person>()
-                .HasMany(e => e.Seminars)
-                .WithMany(e => e.People)
-                .Map(m => m.ToTable("SeminarPersons").MapLeftKey("Person").MapRightKey("Seminar"));
-
-            modelBuilder.Entity<PreView>()
-                .Property(e => e.ShortDescription)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<Specialisation>()
-                .HasMany(e => e.Lectures)
-                .WithRequired(e => e.Specialisation)
-                .HasForeignKey(e => e.SpesialisationId)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Topic>()
-                .HasMany(e => e.Seminars)
-                .WithOptional(e => e.Topic1)
-                .HasForeignKey(e => e.Topic);
+            modelBuilder.RegisterBiography();
+            modelBuilder.RegisterDocument();
+            modelBuilder.RegisterEducation();
+            modelBuilder.RegisterImage();
+            modelBuilder.RegisterLanguage();
+            modelBuilder.RegisterLecture();
+            modelBuilder.RegisterPageType();
+            modelBuilder.RegisterPerson();
+            modelBuilder.RegisterPreView();
+            modelBuilder.RegisterSpecialization();
+            modelBuilder.RegisterTopic();
         }
     }
 }
