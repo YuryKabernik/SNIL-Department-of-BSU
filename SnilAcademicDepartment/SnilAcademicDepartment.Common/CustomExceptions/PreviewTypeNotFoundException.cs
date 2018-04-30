f03d8d7f8394370e20071c4698fd611d01a8cb68
KobernykYura <kobernykbeljr@gmail.com>
2018-04-30 12:27:49 +0300

    Add new PreviewTypeNotFoundException

---
+++ SnilAcademicDepartment/SnilAcademicDepartment.Common/CustomExceptions/PreviewTypeNotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SnilAcademicDepartment.Common.CustomExceptions
+{
+    [Serializable]
+    class PreviewTypeNotFoundException : Exception
+    {
+        public PreviewTypeNotFoundException() : base()
+        {
+
+        }
+
+        public PreviewTypeNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public PreviewTypeNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+        public PreviewTypeNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
