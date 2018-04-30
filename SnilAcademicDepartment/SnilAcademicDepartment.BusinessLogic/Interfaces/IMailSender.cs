8462dc0d5c514171b280fcc763b4eb76488535d9
KobernykYura <kobernykbeljr@gmail.com>
2018-04-30 14:34:15 +0300

    Edit SendingMail service and add interface IMailSender

---
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Interfaces/IMailSender.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SnilAcademicDepartment.BusinessLogic.Interfaces
+{
+    public interface IMailSender
+    {
+        Task SendMailAsync();
+        void SendMail();
+    }
+}
--- SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Services/SendingMail.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Services/SendingMail.cs
@@ -1,6 +1,18 @@
-﻿namespace SnilAcademicDepartment.BusinessLogic.Services
+﻿using System.Threading.Tasks;
+using SnilAcademicDepartment.BusinessLogic.Interfaces;
+
+namespace SnilAcademicDepartment.BusinessLogic.Services
 {
-    class SendingMail
+    public class SendingMail : IMailSender
     {
+        public void SendMail()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SendMailAsync()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
