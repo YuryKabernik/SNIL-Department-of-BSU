8bead9a3f0210771fae288cf9e99a90d6cb1e6cf
KobernykYura <kobernykbeljr@gmail.com>
2018-05-01 09:59:46 +0300

    Revert "Add cookie manager tests"
    This reverts commit 97f29cd24ece60b977e49b670427d0d22d4930af.

--- SnilAcademicDepartment/SnilAcademicDepartmentTests/Services/CookieManagerTests.cs
+++ SnilAcademicDepartment/SnilAcademicDepartmentTests/Services/CookieManagerTests.cs
@@ -1,65 +1,21 @@
-﻿using Moq;
-using NUnit.Framework;
-using SnilAcademicDepartment.Common.LoggerAdapter;
-using SnilAcademicDepartment.DataAccess.Interface;
-using System;
-using System.Globalization;
+﻿using NUnit.Framework;
+using SnilAcademicDepartment.BusinessLogic.Services;
 
 namespace SnilAcademicDepartment.BusinessLogic.Services.Tests
 {
     [TestFixture()]
     public class CookieManagerTests
     {
-        private Mock<IRepository> _repositoryMock;
-        private Mock<NLogAdapter<CookieManagerTests>> _loggerMock;
-        private CookieManager _cookieManager;
-
-        [SetUp]
-        public void SetUpMethod()
+        [Test()]
+        public void CookieManagerTest()
         {
-            this._repositoryMock = new Mock<IRepository>();
-            this._loggerMock = new Mock<NLogAdapter<CookieManagerTests>>();
-            this._cookieManager = new CookieManager(this._loggerMock.Object, this._repositoryMock.Object);
+            Assert.Fail();
         }
 
         [Test()]
-        public void CookieManager_ChangeCultureTest_NullReferenceArgumentThrowsNullReferenceException()
+        public void ChangeCultureTest()
         {
-            Assert.Throws(
-                typeof(NullReferenceException),
-                () => this._cookieManager.ChangeCulture(null));
-        }
-
-        [Test()]
-        public void CookieManager_ChangeCultureTest_BadArgumentThrows()
-        {
-            Assert.Throws(
-                typeof(CultureNotFoundException),
-                () => this._cookieManager.ChangeCulture("asls"));
-        }
-
-        [Test()]
-        public void CookieManager_ChangeCultureTest_DeParameterSetsThreatCultureDe()
-        {
-            var cookieResult = this._cookieManager.ChangeCulture("de");
-            Assert.IsNotNull(cookieResult);
-            Assert.Contains("de", cookieResult.Values);
-        }
-
-        [Test()]
-        public void CookieManager_ChangeCultureTest_EnParameterSetsThreatCultureEn()
-        {
-            var cookieResult = this._cookieManager.ChangeCulture("en");
-            Assert.IsNotNull(cookieResult);
-            Assert.Contains("en", cookieResult.Values);
-        }
-
-        [Test()]
-        public void CookieManager_ChangeCultureTest_RuParameterSetsThreatCultureRu()
-        {
-            var cookieResult = this._cookieManager.ChangeCulture("ru");
-            Assert.IsNotNull(cookieResult);
-            Assert.Contains("ru", cookieResult.Values);
+            Assert.Fail();
         }
     }
 }
