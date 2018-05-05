b4ec71aad03b08185d02b9a99c18d999f1d861a0
KobernykYura <kobernykbeljr@gmail.com>
2018-05-05 16:00:36 +0300

    Add returnUrl generator to cookie controller

--- SnilAcademicDepartment/SnilAcademicDepartment/Controllers/CookiesController.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment/Controllers/CookiesController.cs
@@ -26,25 +26,48 @@ namespace SnilAcademicDepartment.Controllers
         [HttpPost]
         public ActionResult ChangeCulture(string lang)
         {
-            string returnUrl = Request.UrlReferrer.AbsolutePath;
+            string returnUrl = null;
             HttpCookie cookie = null;
 
             try
             {
                 var requestCookie = this.Request.Cookies["language"];
                 cookie = this._cookieManager.SetCookieCulture(lang, requestCookie);
+
+                returnUrl = GenerateReturnUrl(cookie); // Get return Url.
             }
             catch (CultureNotFoundException)
             {
+                returnUrl = Request.UrlReferrer.AbsolutePath;
                 return this.Redirect(returnUrl);
+            }
+            catch(IndexOutOfRangeException)
+            {
+                returnUrl = Request.UrlReferrer.AbsolutePath;
             }
             catch (Exception)
             {
+                returnUrl = Request.UrlReferrer.AbsolutePath;
                 return this.Redirect(returnUrl);
             }
 
             Response.Cookies.Add(cookie);
             return Redirect(returnUrl);
+        }
+
+        /// <summary>
+        /// Reset first segment("language") wich is not "/" of the Url.
+        /// </summary>
+        /// <param name="cookie">Cookie with new culture data.</param>
+        /// <returns>Returns new return Url.</returns>
+        private string GenerateReturnUrl(HttpCookie cookie)
+        {
+            string returnUrl;
+            var segm = Request.UrlReferrer.Segments;
+            segm.SetValue(cookie.Value, 1);
+
+            returnUrl = string.Join<string>("/", segm).Remove(0,1);
+            return returnUrl;
         }
     }
 }
---
+++ SnilAcademicDepartment/SnilAcademicDepartmentTests/Controllers/CookiesControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Moq;
+using NLog;
+using SnilAcademicDepartment.BusinessLogic.Interfaces;
+using System.Web;
+using System;
+
+namespace SnilAcademicDepartment.Controllers.Tests
+{
+    [TestFixture()]
+    public class CookiesControllerTests
+    {
+        private CookiesController _cookieController;
+        private Mock<ILogger> _mockLogger;
+        private Mock<ICookieManager> _mockManager;
+        private Mock<HttpRequestBase> _httpRequest;
+        private Mock<Uri> _httpContext;
+
+        [SetUp()]
+        public void MyTestMethod()
+        {
+            this._mockLogger = new Mock<ILogger>();
+            this._mockManager = new Mock<ICookieManager>();
+            this._cookieController = new CookiesController(this._mockLogger.Object, this._mockManager.Object);
+        }
+
+        [Test()]
+        public void ChangeCultureTest()
+        {
+            var result = this._cookieController.ChangeCulture("en");
+
+            Assert.Fail();
+        }
+    }
+}
