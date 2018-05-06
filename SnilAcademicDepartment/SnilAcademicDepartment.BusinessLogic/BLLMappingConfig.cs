526ca59a4fef82ee97f3bbcc626823da31aac8d2
KobernykYura <kobernykbeljr@gmail.com>
2018-05-06 23:30:27 +0300

    Add preview mapping

---
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/BLLMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SnilAcademicDepartment.DataAccess;
+
+namespace SnilAcademicDepartment.BusinessLogic
+{
+    class BLLMappingConfig : Profile
+    {
+        public BLLMappingConfig()
+        {
+            this.CreateMap<PreView, Models.PreView>()
+                .ForMember(des => des.Title, opt => opt.MapFrom(s => s.Header))
+                .ForMember(des => des.Description, opt => opt.MapFrom(s => s.ShortDescription))
+                .ForMember(des => des.Image, opt => opt.MapFrom(s => s.Image));
+        }
+    }
+}
