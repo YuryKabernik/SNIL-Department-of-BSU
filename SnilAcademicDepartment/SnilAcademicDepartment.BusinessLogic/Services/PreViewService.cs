58c1ae28425b80346ae05ca0d8ae8ca3e7b6c1df
KobernykYura <kobernykbeljr@gmail.com>
2018-05-06 14:48:54 +0300

    Add implementation to preview service

--- SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Services/PreViewService.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Services/PreViewService.cs
@@ -2,6 +2,7 @@
 using SnilAcademicDepartment.BusinessLogic.Interfaces;
 using SnilAcademicDepartment.DataAccess;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace SnilAcademicDepartment.BusinessLogic.Services
@@ -17,8 +18,25 @@ namespace SnilAcademicDepartment.BusinessLogic.Services
             this._repository = repository;
         }
 
+        /// <summary>
+        /// Method of getting firs or default preview of the page type.
+        /// </summary>
+        /// <param name="pageType">Type of the page.</param>
+        /// <returns>Single page preview.</returns>
         public Models.PreView GetPagePreview(string pageType)
         {
+            if (string.IsNullOrEmpty(pageType) || string.IsNullOrWhiteSpace(pageType))
+            {
+                throw new ArgumentNullException(nameof(pageType), "Your argument is Null, Empty or WhiteSpace");
+            }
+
+            var request = this._repository.PreViews.FirstOrDefault(e => e.PageTypeName.PageTypeName == pageType);
+
+            if (request == null)
+            {
+                throw new InvalidOperationException("Cant't find page preview");
+            }
+
             throw new NotImplementedException();
         }
 
