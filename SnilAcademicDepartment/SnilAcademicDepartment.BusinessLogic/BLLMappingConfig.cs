41bd113200c016272ccc4879d445f7bdf0159451
KobernykYura <kobernykbeljr@gmail.com>
2018-05-07 22:16:21 +0300

    Add mapping for EducationBlock and HallOfFame

--- SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/BLLMappingConfig.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/BLLMappingConfig.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using SnilAcademicDepartment.DataAccess;
+using System.Linq;
 
 namespace SnilAcademicDepartment.BusinessLogic
 {
@@ -11,6 +12,16 @@ namespace SnilAcademicDepartment.BusinessLogic
                 .ForMember(des => des.Title, opt => opt.MapFrom(s => s.Header))
                 .ForMember(des => des.Description, opt => opt.MapFrom(s => s.ShortDescription))
                 .ForMember(des => des.Image, opt => opt.MapFrom(s => s.Image));
+
+            this.CreateMap<EducationBlock, Models.KeyAreaBlock>()
+                .ForMember(des => des.Title, opt => opt.MapFrom(s => s.Name))
+                .ForMember(des => des.Description, opt => opt.MapFrom(s => s.Description))
+                .ForMember(des => des.Image, opt => opt.MapFrom(s => s.Image1.Image1))
+                .ForMember(des => des.Topics, opt => opt.MapFrom(s => s.EducationTopics.Select(p=>p.TopicName)));
+
+            this.CreateMap<HallOfFame, Models.Leader>()
+                .ForMember(des => des.Image, opt => opt.MapFrom(s => s.Person.Image.Image1))
+                .ForMember(des => des.FullName, opt => opt.MapFrom(s => s.Person.SecoundName + s.Person.PersonName + s.Person.FathersName));
         }
     }
 }
