c039bba52283fe3284dfaebec7b5ff928b958ce1
KobernykYura <kobernykbeljr@gmail.com>
2018-05-19 14:02:21 +0300

    Add implementation in projects service.

--- SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Interfaces/IProjects.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Interfaces/IProjects.cs
@@ -1,16 +1,34 @@
-﻿using SnilAcademicDepartment.BusinessLogic.Enums;
-using SnilAcademicDepartment.BusinessLogic.DTOModels;
+﻿using SnilAcademicDepartment.BusinessLogic.DTOModels;
 using System.Collections.Generic;
 
 namespace SnilAcademicDepartment.BusinessLogic.Interfaces
 {
     public interface IProjects
     {
-        PreView GetPreView();
+        /// <summary>
+        /// Get first project by type anf language code.
+        /// </summary>
+        /// <param name="projectType"></param>
+        /// <param name="langLCID"></param>
+        /// <returns></returns>
+        Project GetProject(string projectStatus, int langLCID);
 
         /// <summary>
-        /// Get current/finished/conference projects.
+        /// Get all projects.
         /// </summary>
-        IEnumerable<Project> GetProjects(ProjectType projectType);
+        /// <param name="projectType">Status of the project.</param>
+        /// <param name="langLCID"></param>
+        /// <returns></returns>
+        IEnumerable<Project> GetProjects(string projectStatus, int langLCID);
+
+        /// <summary>
+        /// Get number of projects from start to end index.
+        /// </summary>
+        /// <param name="projectType"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="endIndex"></param>
+        /// <param name="langLCID"></param>
+        /// <returns></returns>
+        IEnumerable<Project> GetProjects(string projectStatus, int startIndex, int endIndex, int langLCID);
     }
 }
--- SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Services/ProjectsService.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Services/ProjectsService.cs
@@ -1,9 +1,12 @@
-﻿using NLog;
+﻿using AutoMapper;
+using NLog;
+using SnilAcademicDepartment.BusinessLogic.DTOModels;
 using SnilAcademicDepartment.BusinessLogic.Enums;
 using SnilAcademicDepartment.BusinessLogic.Interfaces;
 using SnilAcademicDepartment.DataAccess;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SnilAcademicDepartment.BusinessLogic.Services
 {
@@ -11,21 +14,76 @@ namespace SnilAcademicDepartment.BusinessLogic.Services
     {
         private readonly ILogger _logger;
         private readonly SnilDBContext _repository;
+        private readonly IMapper _mapper;
 
-        public ProjectsService(ILogger logger, SnilDBContext repository)
+        public ProjectsService(ILogger logger, SnilDBContext repository, IMapper mapper)
         {
             this._logger = logger;
             this._repository = repository;
+            this._mapper = mapper;
         }
 
-        public DTOModels.PreView GetPreView()
+        public DTOModels.Project GetProject(string projectStatus, int langLCID)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(projectStatus) || string.IsNullOrWhiteSpace(projectStatus))
+            {
+                throw new ArgumentNullException(nameof(projectStatus), "Your argument is Null, Empty or WhiteSpace");
+            }
+
+            var requestResult = this._repository.Projects
+                .FirstOrDefault(e => projectStatus.Equals(e.ProjectStatus, StringComparison.OrdinalIgnoreCase)
+                && e.Language.LanguageCode == langLCID);
+
+            if (requestResult == null)
+            {
+                throw new InvalidOperationException("Cant't find project with such options.");
+            }
+
+            return this._mapper.Map<DTOModels.Project>(requestResult);
         }
 
-        public IEnumerable<DTOModels.Project> GetProjects(ProjectType projectType)
+        public IEnumerable<DTOModels.Project> GetProjects(string projectStatus, int langLCID)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(projectStatus) || string.IsNullOrWhiteSpace(projectStatus))
+            {
+                throw new ArgumentNullException(nameof(projectStatus), "Your argument is Null, Empty or WhiteSpace");
+            }
+
+            var requestResult = this._repository.Projects
+                .Where(e => e.ProjectStatus == projectStatus && e.Language.LanguageCode == langLCID)
+                .AsEnumerable();
+
+            if (requestResult == null)
+            {
+                throw new InvalidOperationException("Cant't find projects with such options.");
+            }
+
+            return this._mapper.Map<IEnumerable<DTOModels.Project>>(requestResult);
+        }
+
+        public IEnumerable<DTOModels.Project> GetProjects(string projectStatus, int startIndex, int endIndex, int langLCID)
+        {
+            if (string.IsNullOrEmpty(projectStatus) || string.IsNullOrWhiteSpace(projectStatus))
+            {
+                throw new ArgumentNullException(nameof(projectStatus), "Your argument is Null, Empty or WhiteSpace");
+            }
+
+            if (startIndex <= 0 || endIndex <= 0)
+            {
+                throw new IndexOutOfRangeException("Your start or end index is smaller or equal to zero.");
+            }
+
+            var requestResult = this._repository.Projects
+                .Where(e => e.ProjectStatus == projectStatus && e.Language.LanguageCode == langLCID)
+                .Skip(startIndex).Take(endIndex)
+                .AsEnumerable();
+
+            if (requestResult == null)
+            {
+                throw new InvalidOperationException("Cant't find projects with such options.");
+            }
+
+            return this._mapper.Map<IEnumerable<DTOModels.Project>>(requestResult);
         }
     }
 }
