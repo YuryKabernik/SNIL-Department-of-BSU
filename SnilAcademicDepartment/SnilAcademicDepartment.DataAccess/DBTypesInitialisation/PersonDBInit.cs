cd74092db3089a92e619c6269485c11a46f0c620
KobernykYura <kobernykbeljr@gmail.com>
2018-05-20 10:21:46 +0300

    Add PersonDBInit class

---
+++ SnilAcademicDepartment/SnilAcademicDepartment.DataAccess/DBTypesInitialisation/PersonDBInit.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnilAcademicDepartment.DataAccess.DBTypesInitialisation
+{
+    public static class PersonDBInit
+    {
+        public static void DBInit(
+            SnilDBContext dBContext,
+            string name,
+            string secoundName,
+            string fathers,
+            Biography biogr,
+            Image img,
+            string acadTitle,
+            string degree,
+            Language lang,
+            string status,
+            string interests,
+            Lecture[] lectures,
+            Seminar[] seminars,
+            Project[] projects)
+        {
+            var obj = new Person()
+            {
+                PersonName = name,
+                SecoundName = secoundName,
+                FathersName = fathers,
+                Biography1 = biogr,
+                Image = img,
+                AcademicTitle = acadTitle,
+                Degree = degree,
+                Language = lang,
+                ProfessionStatus = status,
+                PersonalInterests = interests,
+                Lectures = lectures,
+                Seminars = seminars,
+                Projects = projects
+            };
+        }
+    }
+}
