23338a1aca64a4f878f4f6558928cc396e56f671
KobernykYura <kobernykbeljr@gmail.com>
2018-12-16 17:06:14 +0300

    Add Service infrastucture of participants page

---
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Interfaces/IParticipants.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using SnilAcademicDepartment.BusinessLogic.DTOModels;
+
+namespace SnilAcademicDepartment.BusinessLogic.Interfaces
+{
+	/// <summary>
+	/// Interface of SPMA stuff (Students + Persons)
+	/// </summary>
+	public interface ISpmaParticipants
+	{
+		Task<SpmaPerson> GetStuffPersonal();
+		Task<SpmaStudent> GetStuffStudents();
+	}
+}
---
+++ SnilAcademicDepartment/SnilAcademicDepartment.BusinessLogic/Services/ParticipantsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SnilAcademicDepartment.BusinessLogic.Interfaces;
+
+namespace SnilAcademicDepartment.BusinessLogic.Services
+{
+	public class ParticipantsService : ISpmaParticipants
+	{
+	}
+}
