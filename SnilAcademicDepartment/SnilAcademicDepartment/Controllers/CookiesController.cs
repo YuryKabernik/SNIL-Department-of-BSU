2de227a04431c649c170b79455fc31470b01c715
KobernykYura <kobernykbeljr@gmail.com>
2019-07-17 12:39:06 +0300

    Add logging to the CookiesController

--- SnilAcademicDepartment/SnilAcademicDepartment/Controllers/CookiesController.cs
+++ SnilAcademicDepartment/SnilAcademicDepartment/Controllers/CookiesController.cs
@@ -9,75 +9,92 @@ using SnilAcademicDepartment.BusinessLogic.Interfaces;
 
 namespace SnilAcademicDepartment.Controllers
 {
+	/// <summary>
+	/// Cookie management controller.
+	/// </summary>
+	/// <seealso cref="SnilAcademicDepartment.Base.SnilBaseController" />
 	public class CookiesController : SnilBaseController
 	{
-        private readonly ICookieManager _cookieManager;
+		/// <summary>
+		/// The cookie manager
+		/// </summary>
+		private readonly ICookieManager _cookieManager;
 
-        public CookiesController(ILogger logger, ICookieManager cookieManager) : base(logger)
-        {
-            this._cookieManager = cookieManager;
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CookiesController"/> class.
+		/// </summary>
+		/// <param name="logger">The logger.</param>
+		/// <param name="cookieManager">The cookie manager.</param>
+		public CookiesController(ILogger logger, ICookieManager cookieManager) : base(logger)
+		{
+			this._cookieManager = cookieManager;
+		}
 
-        /// <summary>
-        /// Method for changing UI culture by inserted language.
-        /// </summary>
-        /// <param name="lang">User's language.</param>
-        /// <returns>Redirect to previouse page with new language.</returns>
-        [HttpGet]
-        public async Task<ActionResult> ChangeCulture(string lang)
-        {
-            string returnUrl = null;
-            HttpCookie cookie = null;
+		/// <summary>
+		/// Method for changing UI culture by inserted language.
+		/// </summary>
+		/// <param name="lang">User's language.</param>
+		/// <returns>Redirect to previouse page with new language.</returns>
+		[HttpGet]
+		public async Task<ActionResult> ChangeCulture(string lang)
+		{
+			string returnUrl = null;
+			HttpCookie cookie = null;
 
-            try
-            {
-                var requestCookie = this.Request.Cookies["language"];
-                cookie = await this._cookieManager.SetCookieCultureAsync(lang, requestCookie);
+			try
+			{
+				HttpCookie requestCookie = this.Request.Cookies["language"];
+				cookie = await this._cookieManager.SetCookieCultureAsync(lang, requestCookie);
 
-                returnUrl = GenerateReturnUrl(cookie); // Get return Url.
-            }
-            catch (CultureNotFoundException)
-            {
-                returnUrl = Request.UrlReferrer?.AbsoluteUri ?? "/";
-                return this.Redirect(returnUrl);
-            }
-            catch(IndexOutOfRangeException)
-            {
-                returnUrl = Request.UrlReferrer?.AbsoluteUri ?? "/";
-                return this.Redirect(returnUrl);
-            }
-            catch (Exception)
-            {
-                returnUrl = Request.UrlReferrer?.AbsoluteUri ?? "/";
-                return this.Redirect(returnUrl);
-            }
+				returnUrl = GenerateReturnUrl(cookie); // Get return Url.
+			}
+			catch (CultureNotFoundException ex)
+			{
+				string errMessage = $"Change culture error occured in {ex.TargetSite} by stack trace {ex.StackTrace}." +
+					$" Inner exception stack trace {ex.InnerException.StackTrace}";
 
-            Response.Cookies.Add(cookie);
-            return Redirect(returnUrl);
-        }
+				this._logger.Error(ex, errMessage);
 
-        /// <summary>
-        /// Reset first segment("language") wich is not "/" of the Url.
-        /// </summary>
-        /// <param name="cookie">Cookie with new culture data.</param>
-        /// <returns>Returns new return Url.</returns>
-        private string GenerateReturnUrl(HttpCookie cookie)
-        {
-            string returnUrl;
-            var segm = Request.UrlReferrer.Segments;
-            var query = Request.UrlReferrer.Query;
+				returnUrl = Request.UrlReferrer?.AbsoluteUri ?? "/";
+				return this.Redirect(returnUrl);
+			}
+			catch (IndexOutOfRangeException ex)
+			{
+				string errMessage = $"Change culture error occured in {ex.TargetSite} by stack trace {ex.StackTrace}." +
+					$" Inner exception stack trace {ex.InnerException.StackTrace}";
 
-            if (segm.Length != 1)
-            {
-                segm.SetValue(cookie.Value, 1);
-                returnUrl = string.Join<string>("/", segm).Remove(0, 1) + query;
-            }
-            else
-            {
-                returnUrl = segm[0] + cookie.Value;
-            }
+				this._logger.Error(ex, errMessage);
 
-            return returnUrl;
-        }
-    }
+				returnUrl = Request.UrlReferrer?.AbsoluteUri ?? "/";
+				return this.Redirect(returnUrl);
+			}
+
+			Response.Cookies.Add(cookie);
+			return Redirect(returnUrl);
+		}
+
+		/// <summary>
+		/// Reset first segment("language") wich is not "/" of the Url.
+		/// </summary>
+		/// <param name="cookie">Cookie with new culture data.</param>
+		/// <returns>Returns new return Url.</returns>
+		private string GenerateReturnUrl(HttpCookie cookie)
+		{
+			string returnUrl;
+			string[] segm = Request.UrlReferrer.Segments;
+			string query = Request.UrlReferrer.Query;
+
+			if (segm.Length != 1)
+			{
+				segm.SetValue(cookie.Value, 1);
+				returnUrl = string.Join<string>("/", segm).Remove(0, 1) + query;
+			}
+			else
+			{
+				returnUrl = segm[0] + cookie.Value;
+			}
+
+			return returnUrl;
+		}
+	}
 }
